ae198ab7a32f2ef8f0da72a9348db17978445ea0
Hussein Alwash <husseinalosh@gmail.com>
2020-07-18 17:02:16 +0300

    my first app

---
+++ hello.py
@@ -0,0 +1,12 @@
+print('Hi,how are you? ')
+print('Hello !')
+print('''Here's a picture of a dog :
+ o---
+  |||
+  ''')
+born=input('what year were you born? ')
+born=int(born)
+age=2025-born
+print('In the year 2025 you\'ll ',age,'years old!')
+dog=age*7
+print( 'your age in dog years is ',dog)
